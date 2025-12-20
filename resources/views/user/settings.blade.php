a4dda558ab0df8886aee1f2aac044af612727b4f
Olivier van Helden <olivier@van-helden.net>
2025-12-19 23:17:25 -0400

    fix(locales) add missing translations, minor css fixes

--- resources/views/user/settings.blade.php
+++ resources/views/user/settings.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.app')
 
-@section('title', __('app.user_settings'))
+@section('title', __('app.user_account'))
 
 @section('content')
 <div class="max-w-4xl mx-auto">
     <div class="mb-6">
-        <h1 class="text-3xl font-bold text-gray-900">{{ __('app.user_settings') }}</h1>
+        <h1 class="text-3xl font-bold text-gray-900">{{ __('app.user_account') }}</h1>
         <p class="text-gray-600 mt-2">Manage your profile and preferences</p>
     </div>
     
