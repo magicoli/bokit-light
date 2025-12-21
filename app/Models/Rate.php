4202459dfed9eab08e1eeedd7631bbf70e733479
Olivier van Helden <olivier@van-helden.net>
2025-12-21 15:29:09 -0400

    fix(database) fix migration 2025_12_21_200000_update_pricing_structure.php failing and getting retried on each page load

--- app/Models/Rate.php
+++ app/Models/Rate.php
@@ -5,7 +5,6 @@ namespace App\Models;
 use App\Traits\TimezoneTrait;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class Rate extends Model
 {
