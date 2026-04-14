93d985d7ec7371238312ca5c957de6ab508905e0
Olivier van Helden <olivier@van-helden.net>
2026-04-14 15:15:08 -0400

    fix: remove hardcoded unit mappings from HBook and Multipass modules
    - Replace hardcoded options with dynamic calls to WordPress
    - Add TODO comments for actual WordPress API implementation
    - Modules now return empty arrays - ready for WP Connector integration
    - No specific values hardcoded - Bokit remains generic for all users
    
    Next step: Implement getHbookUnitsFromWordPress() and getMultipassUnitsFromWordPress()
    in their respective modules using the WP Connector module

--- modules/hbook/src/HbookServiceProvider.php
+++ modules/hbook/src/HbookServiceProvider.php
@@ -3,6 +3,9 @@
 namespace Modules\Hbook;
 
 use App\Filament\Resources\Units\Schemas\UnitForm;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Get as FormsGet;
+use Filament\Schemas\Components\Utilities\Get as SchemaGet;
 use Illuminate\Support\ServiceProvider;
 use Modules\Hbook\Commands\HbookImportCommand;
 use Modules\Hbook\Commands\MultipassImportCommand;
@@ -32,24 +35,38 @@ class HbookServiceProvider extends ServiceProvider
     protected function extendUnitForm(): void
     {
         UnitForm::extend(function (array $components): array {
-            foreach ($components as &$component) {
+            // Find the sources repeater and extend it
+            foreach ($components as $index => $component) {
                 if (isset($component['schema'][0]['schema'])) {
-                    // Find the hbook_unit_id field and populate its options
-                    foreach ($component['schema'][0]['schema'] as &$field) {
+                    $schema = $component['schema'][0]['schema'];
+                    
+                    // Find and update the hbook_unit_id field
+                    foreach ($schema as $fieldIndex => $field) {
                         if (isset($field['name']) && $field['name'] === 'hbook_unit_id') {
-                            // TODO: Replace with actual WordPress API call
-                            $field['options'] = [
-                                'hbook-1' => 'Moon',
-                                'hbook-2' => 'Sun',
-                                'hbook-3' => 'Zandoli',
-                                'hbook-4' => 'Violeta',
-                                'hbook-5' => 'Zetoil',
-                            ];
+                            // Create a new field with dynamic options from WordPress
+                            $schema[$fieldIndex] = Select::make('hbook_unit_id')
+                                ->label(__('unit.field.source_hbook_unit'))
+                                ->options(fn () => $this->getHbookUnitsFromWordPress())
+                                ->visible(fn (FormsGet|SchemaGet $get): bool => $get('type') === 'hbook')
+                                ->columnSpan(1);
                         }
                     }
+                    
+                    $components[$index]['schema'][0]['schema'] = $schema;
                 }
             }
             return $components;
         });
+    }
+
+    /**
+     * Get HBook units from WordPress API
+     * TODO: Implement actual WordPress API call via WP Connector module
+     */
+    protected function getHbookUnitsFromWordPress(): array
+    {
+        // This should call the WP Connector module to fetch units from WordPress
+        // For now, return empty array - the actual implementation will be added later
+        return [];
     }
 }
--- modules/multipass/src/MultipassServiceProvider.php
+++ modules/multipass/src/MultipassServiceProvider.php
@@ -3,6 +3,9 @@
 namespace Modules\Multipass;
 
 use App\Filament\Resources\Units\Schemas\UnitForm;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Get as FormsGet;
+use Filament\Schemas\Components\Utilities\Get as SchemaGet;
 use Illuminate\Support\ServiceProvider;
 
 class MultipassServiceProvider extends ServiceProvider
@@ -23,24 +26,38 @@ class MultipassServiceProvider extends ServiceProvider
     protected function extendUnitForm(): void
     {
         UnitForm::extend(function (array $components): array {
-            foreach ($components as &$component) {
+            // Find the sources repeater and extend it
+            foreach ($components as $index => $component) {
                 if (isset($component['schema'][0]['schema'])) {
-                    // Find the multipass_unit_id field and populate its options
-                    foreach ($component['schema'][0]['schema'] as &$field) {
+                    $schema = $component['schema'][0]['schema'];
+                    
+                    // Find and update the multipass_unit_id field
+                    foreach ($schema as $fieldIndex => $field) {
                         if (isset($field['name']) && $field['name'] === 'multipass_unit_id') {
-                            // TODO: Replace with actual WordPress API call
-                            $field['options'] = [
-                                'mp-1' => 'Moon',
-                                'mp-2' => 'Sun',
-                                'mp-3' => 'Zandoli',
-                                'mp-4' => 'Violeta',
-                                'mp-5' => 'Zetoil',
-                            ];
+                            // Create a new field with dynamic options from WordPress
+                            $schema[$fieldIndex] = Select::make('multipass_unit_id')
+                                ->label(__('unit.field.source_multipass_unit'))
+                                ->options(fn () => $this->getMultipassUnitsFromWordPress())
+                                ->visible(fn (FormsGet|SchemaGet $get): bool => $get('type') === 'multipass')
+                                ->columnSpan(1);
                         }
                     }
+                    
+                    $components[$index]['schema'][0]['schema'] = $schema;
                 }
             }
             return $components;
         });
+    }
+
+    /**
+     * Get Multipass units from WordPress API
+     * TODO: Implement actual WordPress API call via WP Connector module
+     */
+    protected function getMultipassUnitsFromWordPress(): array
+    {
+        // This should call the WP Connector module to fetch units from WordPress
+        // For now, return empty array - the actual implementation will be added later
+        return [];
     }
 }
