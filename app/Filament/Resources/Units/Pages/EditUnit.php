7328394b9a08a91769b969412572cc7aea0172ac
Olivier van Helden <olivier@van-helden.net>
2026-04-14 13:38:16 -0400

    fix: simplify EditUnit page and remove unnecessary trait

--- app/Filament/Resources/Units/Pages/EditUnit.php
+++ app/Filament/Resources/Units/Pages/EditUnit.php
@@ -6,12 +6,9 @@ use App\Filament\Resources\Units\UnitResource;
 use Filament\Actions\DeleteAction;
 use Filament\Actions\ViewAction;
 use Filament\Resources\Pages\EditRecord;
-use Filament\Resources\Pages\EditRecord\Concerns\HasRelationManagers;
 
 class EditUnit extends EditRecord
 {
-    use HasRelationManagers;
-
     protected static string $resource = UnitResource::class;
 
     protected function getHeaderActions(): array
@@ -21,17 +18,6 @@ class EditUnit extends EditRecord
 
     protected function getActions(): array
     {
-        return [
-            ViewAction::make(),
-            DeleteAction::make(),
-        ];
-    }
-
-    /**
-     * Use full width for the edit page
-     */
-    protected function getMaxContentWidth(): string
-    {
-        return 'full';
+        return [ViewAction::make(), DeleteAction::make()];
     }
 }
