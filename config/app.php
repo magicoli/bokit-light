55724882a7366d37a9fed92a2e3d698e2b3e5dff
Olivier van Helden <olivier@van-helden.net>
2026-07-27 01:18:31 -0400

    (style) reformat config/app.php with mago

--- config/app.php
+++ config/app.php
@@ -1,142 +1,140 @@
 <?php
 
 // Generate a random key and save it if not provided
-$key = env("APP_KEY", "base64:" . base64_encode(random_bytes(32)));
-if (empty(env("APP_KEY"))) {
+$key = env('APP_KEY', 'base64:' . base64_encode(random_bytes(32)));
+if (empty(env('APP_KEY'))) {
     putenv("APP_KEY={$key}");
 
-    if (!file_exists($envPath = dirname(__DIR__) . "/.env")) {
+    if (!file_exists($envPath = dirname(__DIR__) . '/.env')) {
         file_put_contents($envPath, "APP_KEY={$key}\n");
     } else {
         // Replace APP_KEY in .env file if it exists, otherwise add it
         $env = file_get_contents($envPath);
-        $env =
-            "APP_KEY={$key}\n" .
-            preg_replace('/^APP_KEY=.*$(?:\r\n|\n)?/m', "", $env);
+        $env = "APP_KEY={$key}\n" . preg_replace('/^APP_KEY=.*$(?:\r\n|\n)?/m', '', $env);
         file_put_contents($envPath, $env);
     }
 }
 
 return [
     /*
-    |--------------------------------------------------------------------------
-    | Application Name
-    |--------------------------------------------------------------------------
-    |
-    | This value is the name of your application, which will be used when the
-    | framework needs to place the application's name in a notification or
-    | other UI elements where an application name needs to be displayed.
-    |
-    */
+     |--------------------------------------------------------------------------
+     | Application Name
+     |--------------------------------------------------------------------------
+     |
+     | This value is the name of your application, which will be used when the
+     | framework needs to place the application's name in a notification or
+     | other UI elements where an application name needs to be displayed.
+     |
+     */
 
-    "name" => localAppName(),
-    "slogan" => env("APP_SLOGAN", "Bring On Kitsch Island Time"),
-    "logo" => env("APP_LOGO", "/images/logo.png"),
-    "version" => env("APP_VERSION", "1.0.0"),
-
-    /*
-    |--------------------------------------------------------------------------
-    | Application Environment
-    |--------------------------------------------------------------------------
-    |
-    | This value determines the "environment" your application is currently
-    | running in. This may determine how you prefer to configure various
-    | services the application utilizes. Set this in your ".env" file.
-    |
-    */
-
-    "env" => env("APP_ENV", isLocal() ? "local" : "production"),
+    'name' => localAppName(),
+    'slogan' => env('APP_SLOGAN', 'Bring On Kitsch Island Time'),
+    'logo' => env('APP_LOGO', '/images/logo.png'),
+    'version' => env('APP_VERSION', '0.1.2'),
 
     /*
-    |--------------------------------------------------------------------------
-    | Application Debug Mode
-    |--------------------------------------------------------------------------
-    |
-    | When your application is in debug mode, detailed error messages with
-    | stack traces will be shown on every error that occurs within your
-    | application. If disabled, a simple generic error page is shown.
-    |
-    */
+     |--------------------------------------------------------------------------
+     | Application Environment
+     |--------------------------------------------------------------------------
+     |
+     | This value determines the "environment" your application is currently
+     | running in. This may determine how you prefer to configure various
+     | services the application utilizes. Set this in your ".env" file.
+     |
+     */
 
-    "debug" => (bool) env("APP_DEBUG", false),
-
-    /*
-    |--------------------------------------------------------------------------
-    | Application URL
-    |--------------------------------------------------------------------------
-    |
-    | This URL is used by the console to properly generate URLs when using
-    | the Artisan command line tool. You should set this to the root of
-    | the application so that it's available within Artisan commands.
-    |
-    */
-
-    "url" => env("APP_URL", "http://localhost"),
+    'env' => env('APP_ENV', isLocal() ? 'local' : 'production'),
 
     /*
-    |--------------------------------------------------------------------------
-    | Application Timezone
-    |--------------------------------------------------------------------------
-    |
-    | Here you may specify the default timezone for your application, which
-    | will be used by the PHP date and date-time functions. The timezone
-    | is set to "UTC" by default as it is suitable for most use cases.
-    |
-    */
+     |--------------------------------------------------------------------------
+     | Application Debug Mode
+     |--------------------------------------------------------------------------
+     |
+     | When your application is in debug mode, detailed error messages with
+     | stack traces will be shown on every error that occurs within your
+     | application. If disabled, a simple generic error page is shown.
+     |
+     */
 
-    "timezone" => "UTC",
-
-    /*
-    |--------------------------------------------------------------------------
-    | Application Locale Configuration
-    |--------------------------------------------------------------------------
-    |
-    | The application locale determines the default locale that will be used
-    | by Laravel's translation / localization methods. This option can be
-    | set to any locale for which you plan to have translation strings.
-    |
-    */
-
-    "locale" => env("APP_LOCALE", "en"),
-
-    "fallback_locale" => env("APP_FALLBACK_LOCALE", "en"),
-
-    "faker_locale" => env("APP_FAKER_LOCALE", "en_US"),
+    'debug' => (bool) env('APP_DEBUG', false),
 
     /*
-    |--------------------------------------------------------------------------
-    | Encryption Key
-    |--------------------------------------------------------------------------
-    |
-    | This key is utilized by Laravel's encryption services and should be set
-    | to a random, 32 character string to ensure that all encrypted values
-    | are secure. You should do this prior to deploying the application.
-    |
-    */
+     |--------------------------------------------------------------------------
+     | Application URL
+     |--------------------------------------------------------------------------
+     |
+     | This URL is used by the console to properly generate URLs when using
+     | the Artisan command line tool. You should set this to the root of
+     | the application so that it's available within Artisan commands.
+     |
+     */
 
-    "cipher" => "AES-256-CBC",
+    'url' => env('APP_URL', 'http://localhost'),
 
-    "key" => env("APP_KEY"),
+    /*
+     |--------------------------------------------------------------------------
+     | Application Timezone
+     |--------------------------------------------------------------------------
+     |
+     | Here you may specify the default timezone for your application, which
+     | will be used by the PHP date and date-time functions. The timezone
+     | is set to "UTC" by default as it is suitable for most use cases.
+     |
+     */
 
-    "previous_keys" => [
-        ...array_filter(explode(",", (string) env("APP_PREVIOUS_KEYS", ""))),
+    'timezone' => 'UTC',
+
+    /*
+     |--------------------------------------------------------------------------
+     | Application Locale Configuration
+     |--------------------------------------------------------------------------
+     |
+     | The application locale determines the default locale that will be used
+     | by Laravel's translation / localization methods. This option can be
+     | set to any locale for which you plan to have translation strings.
+     |
+     */
+
+    'locale' => env('APP_LOCALE', 'en'),
+
+    'fallback_locale' => env('APP_FALLBACK_LOCALE', 'en'),
+
+    'faker_locale' => env('APP_FAKER_LOCALE', 'en_US'),
+
+    /*
+     |--------------------------------------------------------------------------
+     | Encryption Key
+     |--------------------------------------------------------------------------
+     |
+     | This key is utilized by Laravel's encryption services and should be set
+     | to a random, 32 character string to ensure that all encrypted values
+     | are secure. You should do this prior to deploying the application.
+     |
+     */
+
+    'cipher' => 'AES-256-CBC',
+
+    'key' => env('APP_KEY'),
+
+    'previous_keys' => [
+        ...array_filter(explode(',', (string) env('APP_PREVIOUS_KEYS', ''))),
     ],
 
     /*
-    |--------------------------------------------------------------------------
-    | Maintenance Mode Driver
-    |--------------------------------------------------------------------------
-    |
-    | These configuration options determine the driver used to determine and
-    | manage Laravel's "maintenance mode" status. The "cache" driver will
-    | allow maintenance mode to be controlled across multiple machines.
-    |
-    | Supported drivers: "file", "cache"
-    |
-    */
+     |--------------------------------------------------------------------------
+     | Maintenance Mode Driver
+     |--------------------------------------------------------------------------
+     |
+     | These configuration options determine the driver used to determine and
+     | manage Laravel's "maintenance mode" status. The "cache" driver will
+     | allow maintenance mode to be controlled across multiple machines.
+     |
+     | Supported drivers: "file", "cache"
+     |
+     */
 
-    "maintenance" => [
-        "driver" => env("APP_MAINTENANCE_DRIVER", "file"),
-        "store" => env("APP_MAINTENANCE_STORE", "database"),
+    'maintenance' => [
+        'driver' => env('APP_MAINTENANCE_DRIVER', 'file'),
+        'store' => env('APP_MAINTENANCE_STORE', 'database'),
     ],
 ];
